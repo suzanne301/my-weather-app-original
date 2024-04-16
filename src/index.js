357dc673a2d2e1f842b98d0892e9722ad1fa332c
suzanne301 <104364608+suzanne301@users.noreply.github.com>
2024-04-17 00:15:12 +0100

    Injected the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -46,7 +46,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml +=
+      `  
+      <div class="weather-forecast-day">      
+        <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">⛅️</div>
+          <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">18°</span>
+          <span class="weather-forecast-temperature-min">12°</span>
+        </div>
+      </div>
+      `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Haddington");
+displayForecast();
